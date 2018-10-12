d3d4efb8c90b46dcd536e76de59b9bdeb8bff648
serezhishe <43710015+serezhishe@users.noreply.github.com>
2018-10-13 00:07:49 +0300

    Update 2048.cpp

--- komposterV2/2048.cpp
+++ komposterV2/2048.cpp
@@ -1,4 +1,4 @@
-﻿#include "pch.h"
+#include "pch.h"
 #include <iostream>
 #include <cstdlib>
 #include <dos.h>
@@ -27,7 +27,7 @@ void delete_matrix(int** matrix_to_delete) {
 }
 
 void draw_field(int** matrix_to_draw) {
-	system("CLS");
+
 	cout << endl;
 	for (int i = 0; i < GLOBAL_LENGTH; i++) {
 		for (int j = 0; j < GLOBAL_LENGTH; j++) {
@@ -54,159 +54,86 @@ void init_game(int** matrix) {
 	}
 }
 
-void move_left(int** field) {
-	for (int i = 0; i < GLOBAL_LENGTH; i++) {
-		for (int j = 1; j < GLOBAL_LENGTH; j++) {
-			if (field[i][j - 1] == 0) {
-				field[i][j - 1] = field[i][j];
+bool check_borders(int position, int hernya) {
+	return (position + hernya >= 0) && (position + hernya < GLOBAL_LENGTH);
+}
+
+void move_throw_zeros(int** field, int x1,int y1, int huy) {
+	int tmp1 = fabs((GLOBAL_LENGTH - 1)*x1 - (GLOBAL_LENGTH - 1)) / 2 + x1*huy;
+	int tmp2 = fabs((GLOBAL_LENGTH - 1)*y1 - (GLOBAL_LENGTH - 1)) / 2 + y1*(1 - huy);
+	for (int i = tmp1; check_borders(i, -x1*huy) && check_borders(i, 0); i+=x1) {
+		for (int j = tmp2; check_borders(j, -y1*(1-huy)) && check_borders(j, 0); j+=y1) {
+			if (field[i - x1*huy][j - y1*(1-huy)] == 0) {
+				field[i - x1*huy][j - y1*(1-huy)] = field[i][j];
 				field[i][j] = 0;
-				for (int k = j; k > 0; k--) {
-					if (field[i][k - 1] == 0) {
-						field[i][k - 1] = field[i][k];
-						field[i][k] = 0;
+				if (huy) {
+					for (int k = i; check_borders(k, -x1) && check_borders(k, 0); k-=x1) {
+						if (field[k - x1][j] == 0) {
+							field[k - x1][j] = field[k][j];
+							field[k][j] = 0;
+						}
+					}
+				}
+				else {
+					for (int k = j; check_borders(k, -y1) && check_borders(k, 0); k -= y1) {
+						if (field[i][k - y1] == 0) {
+							field[i][k - y1] = field[i][k];
+							field[i][k] = 0;
+						}
 					}
 				}
 			}
 		}
 	}
+}
+
+void move_left(int** field) {
+	move_throw_zeros(field,  1, 1, 0);
 	for (int i = 0; i < GLOBAL_LENGTH; i++) {
 		for (int j = 1; j < GLOBAL_LENGTH; j++) {
 			if (field[i][j] == field[i][j - 1]) {
 				field[i][j - 1] *= 2;
 				field[i][j] = 0;
-				for (int f = 0; f < GLOBAL_LENGTH; f++) {
-					for (int l = 1; l < GLOBAL_LENGTH; l++) {
-						if (field[f][l - 1] == 0) {
-							field[f][l - 1] = field[f][l];
-							field[f][l] = 0;
-							for (int k = l; k > 0; k--) {
-								if (field[f][k - 1] == 0) {
-									field[f][k - 1] = field[f][k];
-									field[f][k] = 0;
-								}
-							}
-						}
-					}
-				}
-				j++;
+				move_throw_zeros(field, 1, 1, 0);
 			}
 		}
 	}
 }
 
 void move_right(int** field) {
-	for (int i = 0; i < GLOBAL_LENGTH; i++) {
-		for (int j = GLOBAL_LENGTH - 2; j >= 0; j--) {
-			if (field[i][j + 1] == 0) {
-				field[i][j + 1] = field[i][j];
-				field[i][j] = 0;
-			}
-		}
-	}
+	move_throw_zeros(field, 1, -1, 0);
 	for (int i = 0; i < GLOBAL_LENGTH; i++) {
 		for (int j = GLOBAL_LENGTH - 2; j >= 0; j--) {
 			if (field[i][j] == field[i][j + 1]) {
 				field[i][j + 1] *= 2;
 				field[i][j] = 0;
-				for (int f = 0; f < GLOBAL_LENGTH; f++) {
-					for (int l = GLOBAL_LENGTH - 2; l >= 0; l--) {
-						if (field[f][l + 1] == 0) {
-							field[f][l + 1] = field[f][l];
-							field[f][l] = 0;
-							for (int k = l; k > 0; k--) {
-								if (field[f][k + 1] == 0) {
-									field[f][k + 1] = field[f][k];
-									field[f][k] = 0;
-								}
-							}
-						}
-					}
-					j--;
-				}
+				move_throw_zeros(field, 1, -1, 0);
 			}
 		}
 	}
 }
 
 void move_up(int** field) {
-
-	for (int j = 0; j < GLOBAL_LENGTH; j++) {
-		for (int i = 1; i < GLOBAL_LENGTH; i++) {
-			if (field[i - 1][j] == 0) {
-				field[i - 1][j] = field[i][j];
-				field[i][j] = 0;
-				for (int k = i; k > 0; k--) {
-					if (field[k - 1][j] == 0) {
-						field[k - 1][j] = field[k][j];
-						field[k][j] = 0;
-
-					}
-				}
-			}
-		}
-	}
+	move_throw_zeros(field, 1, 1, 1);
 	for (int j = 0; j < GLOBAL_LENGTH; j++) {
 		for (int i = 1; i < GLOBAL_LENGTH; i++) {
 			if (field[i][j] == field[i - 1][j]) {
 				field[i - 1][j] *= 2;
 				field[i][j] = 0;
-				for (int f = 0; f < GLOBAL_LENGTH; f++) {
-					for (int l = 1; l < GLOBAL_LENGTH; l++) {
-						if (field[l - 1][f] == 0) {
-							field[l - 1][f] = field[l][f];
-							field[l][f] = 0;
-							for (int k = l; k > 0; k--) {
-								if (field[l - 1][k] == 0) {
-									field[l - 1][k] = field[l][k];
-									field[l][k] = 0;
-								}
-							}
-						}
-					}
-				}
-				i++;
+				move_throw_zeros(field, 1, 1, 1);
 			}
 		}
 	}
 }
 
 void move_down(int** field) {
-
-	for (int j = 0; j < GLOBAL_LENGTH; j++) {
-		for (int i = GLOBAL_LENGTH - 2; i >= 0; i--) {
-			if (field[i + 1][j] == 0) {
-				field[i + 1][j] = field[i][j];
-				field[i][j] = 0;
-				for (int k = i; k < GLOBAL_LENGTH - 1; k++) {
-					if (field[k + 1][j] == 0) {
-						field[k + 1][j] = field[k][j];
-						field[k][j] = 0;
-
-					}
-				}
-			}
-		}
-	}
+	move_throw_zeros(field, -1, 1, 1);
 	for (int j = 0; j < GLOBAL_LENGTH; j++) {
 		for (int i = GLOBAL_LENGTH - 2; i >= 0; i--) {
 			if (field[i][j] == field[i + 1][j]) {
 				field[i + 1][j] *= 2;
 				field[i][j] = 0;
-				for (int f = 0; f < GLOBAL_LENGTH; f++) {
-					for (int l = GLOBAL_LENGTH - 2; l >= 0; l--) {
-						if (field[l + 1][f] == 0) {
-							field[l + 1][f] = field[l][f];
-							field[l][f] = 0;
-							for (int k = l; k > 0; k--) {
-								if (field[l+1][k] == 0) {
-									field[l + 1][k] = field[l][k];
-									field[l][k] = 0;
-								}
-							}
-						}
-					}
-				}
-				i--;
+				move_throw_zeros(field, -1, 1, 1);
 			}
 		}
 	}
@@ -264,10 +191,6 @@ bool field_full(int** field) {
 	}
 	return 1;
 }
-bool check_borders(int position, int hernya) {
-	return (position + hernya >= 0) && (position + hernya < GLOBAL_LENGTH);
-}
-
 bool check_same_neighbours(int x_position, int y_position, int** current_state) {
 	for (int i = -1; i < 2; i++) {
 		for (int j = -1; j < 2; j++) {
@@ -299,10 +222,11 @@ void game_loop() {
 	int** game_field = init_matrix();
 	int** previous_state = init_matrix();
 	init_game(game_field);
+	draw_field(game_field);
 	while (!game_end(game_field)) {
-		draw_field(game_field);
 		apply_rules(game_field, previous_state);
 		update_previous_state(previous_state, game_field);
+		draw_field(game_field);
 	}
 	cout << "YOU ARE LOSER";
 	delete_matrix(game_field);
